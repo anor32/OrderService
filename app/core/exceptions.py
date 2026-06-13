49babfc24b53d6213efb814f56de9853179a74fa
anor23 <burgchiken@gmail.com>
2026-06-13 21:31:12 +0300

    feat: init alembic migrations

---
+++ app/core/exceptions.py
@@ -0,0 +1,10 @@
+class ObjectNotFound(Exception):
+    def __init__(self, message: str = "Объект не найден"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class IdempotencyError(Exception):
+    def __init__(self, message: str = "Ошибка идемпотентности"):
+        self.message = message
+        super().__init__(self.message)
