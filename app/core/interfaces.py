06f6f2d222f678739be999d17eec49da6dc4dcae
anor23 <burgchiken@gmail.com>
2026-06-13 10:59:28 +0300

    feat: add repository interfaces and UnitOfWork interface

---
+++ app/core/interfaces.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from app.core.schemas import Order
+
+
+class OrderRepository(ABC):
+    class CreateDTO(BaseModel):
+        user_id: str
+        item_id: UUID
+        quantity: int
+
+    @abstractmethod
+    def create(self, order: CreateDTO) -> None:
+        pass
+
+    @abstractmethod
+    def get_by_id(self, order_id: str) -> Order | None:
+        pass
+
+
+class OutboxRepository(ABC):
+    @abstractmethod
+    def create_outbox(self, outbox):
+        pass
+
+    @abstractmethod
+    def get_records(self):
+        pass
+
+
+class UnitOfWorkBase(ABC):
+    @abstractmethod
+    async def __aenter__(self):
+        pass
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    @abstractmethod
+    async def commit(self):
+        pass
+
+    @abstractmethod
+    async def rollback(self):
+        pass
+
+    @abstractmethod
+    async def close(self):
+        pass
+
+
+class UnitOfWork(UnitOfWorkBase):
+    outbox_repo: OutboxRepository
+    order_repo: OrderRepository
