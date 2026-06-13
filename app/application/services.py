a622b1f1c2943b9771507363654ac62f3ed24066
anor23 <burgchiken@gmail.com>
2026-06-13 19:36:11 +0300

    feat: update create order add transactional save on db

--- app/application/services.py
+++ app/application/services.py
@@ -1,22 +1,38 @@
 from app.core.interfaces import UnitOfWork
-from app.core.schemas import CreateOrderRequest
+from app.core.schemas import CreateOrderRequest, Order
 
 
 class OrderService:
     def __init__(self, uow: UnitOfWork):
         self.uow = uow
 
-    async def create_order(self, body: CreateOrderRequest):
+    async def create_order(self, order: CreateOrderRequest) -> Order:
+        inbox = await self.uow.inbox_repo.get_by_idempotency_key(
+            order.idempotency_key
+        )
+        if inbox:
+            await self.uow.inbox_repo.check_idempotency(
+                key=inbox.idempotency_key, payload=order.model_dump()
+            )
         dto = self.uow.order_repo.CreateDTO(
-            user_id=body.user_id, item_id=body.item_id, quantity=body.quantity
+            user_id=order.user_id,
+            item_id=order.item_id,
+            quantity=order.quantity,
         )
         outbox_dto = self.uow.outbox_repo.CreateDTO(
             event_type="create_order", payload=dto.model_dump()
         )
         try:
             async with self.uow as u:
-                await u.order_repo.create(dto)
+                result = await u.order_repo.create(dto)
                 await u.outbox_repo.create_outbox(outbox_dto)
-
+                inbox_dto = u.inbox_repo.CreateDTO(
+                    idempotency_key=order.idempotency_key,
+                    payload=order.model_dump(),
+                    result=result.model_dump(),
+                )
+                await u.inbox_repo.save(inbox_dto)
         except Exception as e:
             raise e
+
+        return result
