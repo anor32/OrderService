32f1733b54362b1245b8f8915825e59cbcfd3005
anor23 <burgchiken@gmail.com>
2026-06-13 21:27:55 +0300

    feat: add main for launch project

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import logging
+
+from fastapi import FastAPI
+
+from app.presentation.routers import router
+
+logger = logging.getLogger(__name__)
+
+
+app = FastAPI()
+# app.add_middleware(ErrorHandlingMiddleware)
+# app.add_middleware(MetricsMiddleware)
+app.include_router(router)
