138dc1cd95498138cec61b2c10569d170f4b3a6a
anor23 <burgchiken@gmail.com>
2026-06-13 20:23:13 +0300

    fix: UUID wrong type in models db

--- app/infrastructure/db/models.py
+++ app/infrastructure/db/models.py
@@ -1,7 +1,7 @@
-import uuid
 from datetime import datetime
+from uuid import UUID, uuid4
 
-from sqlalchemy import JSON, UUID, DateTime, String
+from sqlalchemy import JSON, DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.schemas import InboxStatus, OrderStatus, OutboxStatus
@@ -11,7 +11,7 @@ from app.infrastructure.db.db_config import Base
 class OrderModel(Base):
     __tablename__ = "orders"
 
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     user_id: Mapped[str] = mapped_column(String(255), nullable=False)
     item_id: Mapped[UUID] = mapped_column(nullable=False)
     quantity: Mapped[int] = mapped_column(nullable=False)
@@ -19,27 +19,23 @@ class OrderModel(Base):
         String(50), nullable=False, default=OrderStatus.NEW
     )
     created_at: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, default=datetime.now()
+        DateTime, nullable=False, default=datetime.now
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, default=datetime.now()
+        DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
     )
 
 
 class OutboxModel(Base):
     __tablename__ = "outbox"
-    id: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True
-    )
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     event_type: Mapped[str] = mapped_column(String(100), nullable=False)
     payload: Mapped[dict] = mapped_column(JSON, nullable=False)
     status: Mapped[OutboxStatus] = mapped_column(
-        String(20),
-        default=OutboxStatus.PENDING,
-        nullable=False,
+        String(20), default=OutboxStatus.PENDING, nullable=False
     )
     created_at: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.now(), nullable=False
+        DateTime, default=datetime.now, nullable=False
     )
     sent_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
 
@@ -47,7 +43,7 @@ class OutboxModel(Base):
 class InboxModel(Base):
     __tablename__ = "inbox"
     idempotency_key: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True
+        primary_key=True, default=uuid4
     )
     status: Mapped[InboxStatus] = mapped_column(
         default=InboxStatus.PENDING, nullable=False
