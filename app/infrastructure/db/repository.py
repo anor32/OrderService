4126488e884371c5701431b6675fc492425587fa
anor23 <burgchiken@gmail.com>
2026-06-13 14:24:25 +0300

    feat: add outboxRepositoryImpl implementation interface

--- app/infrastructure/db/repository.py
+++ app/infrastructure/db/repository.py
@@ -1,25 +1,20 @@
+from datetime import datetime
 from uuid import UUID
 
 from db_config import Session
-from pydantic import BaseModel
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 from app.core.exceptions import ObjectNotFound
-from app.core.interfaces import OrderRepository
-from app.core.schemas import Order
-from app.infrastructure.db.models import OrderModel
+from app.core.interfaces import OrderRepository, OutboxRepository
+from app.core.schemas import Order, Outbox
+from app.infrastructure.db.models import OrderModel, OutboxModel, OutboxStatus
 
 
 class OrderRepositoryImpl(OrderRepository):
-    class CreateDTO(BaseModel):
-        user_id: str
-        item_id: UUID
-        quantity: int
-
     def __init__(self, session: Session):
         self._session = session
 
-    async def create(self, data: CreateDTO):
+    async def create(self, data: OrderRepository.CreateDTO):
         order = OrderModel(**data.model_dump())
         await self._session.add(order)
 
@@ -40,3 +35,41 @@ class OrderRepositoryImpl(OrderRepository):
             status=model.status,
         )
         return order
+
+
+class OutboxRepositoryImpl(OutboxRepository):
+    def __init__(self, session: Session):
+        self._session = session
+
+    async def create_outbox(self, outbox: OutboxRepository.CreateDTO):
+        out = OutboxModel(**outbox.model_dump())
+        await self._session.add(out)
+
+    async def get_records(self) -> list[Outbox]:
+        records = await (
+            self._session.scalars(OutboxModel)
+            .where(OutboxModel.status == OutboxStatus.PENDING)
+            .all()
+        )
+        records = [self._construct(record) for record in records]
+
+        return records
+
+    def set_sent_status(self, ids: list[UUID]):
+        stmt = (
+            update(OutboxModel)
+            .where(OutboxModel.id.in_(ids))
+            .values(status=OutboxStatus.SENT, sent_at=datetime.now())
+        )
+        self._session.execute(stmt)
+
+    def _construct(self, model: OutboxModel) -> Outbox:
+        outbox = Outbox(
+            status=model.status,
+            event_type=model.event_type,
+            payload=model.payload,
+            created_at=model.created_at,
+            sent_at=model.sent_at,
+        )
+
+        return outbox
