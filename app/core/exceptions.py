a9e53fd159e845b5bec5acb072e32bb656d4fb64
anor23 <burgchiken@gmail.com>
2026-06-14 17:07:27 +0300

    feat: add new exceptions type

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -8,3 +8,11 @@ class IdempotencyError(Exception):
     def __init__(self, message: str = "Ошибка идемпотентности"):
         self.message = message
         super().__init__(self.message)
+
+
+class NotEnoughStockError(Exception):
+    def __init__(
+        self, message: str = "количество товара на складе не достаточно"
+    ):
+        self.message = message
+        super().__init__(self.message)
