c552cbabc69c254974bea8b0c2260681c9d62da7
anor23 <burgchiken@gmail.com>
2026-06-14 17:26:57 +0300

    refactor: split Order service on transactional_save and check_idempotency

--- app/application/services.py
+++ app/application/services.py
@@ -1,12 +1,24 @@
+from datetime import datetime
+
 from app.core.interfaces import UnitOfWork
-from app.core.schemas import CreateOrderRequest, Order, Outbox
+from app.core.schemas import CreateOrderRequest, Order, OutboxStatus
 
 
 class OrderService:
     def __init__(self, uow: UnitOfWork):
         self.uow = uow
 
-    async def create_order(self, order: CreateOrderRequest) -> Order:
+    async def create_order(self, order_request: CreateOrderRequest) -> Order:
+        inbox = await self._check_idempotency(order_request)
+        if inbox:
+            return inbox
+
+        result = await self._transactional_save(order_request)
+        return result
+
+    async def _check_idempotency(
+        self, order: CreateOrderRequest
+    ) -> Order | None:
         inbox = await self.uow.inbox_repo.get_by_idempotency_key(
             order.idempotency_key
         )
@@ -16,32 +28,33 @@ class OrderService:
                 payload=order.model_dump(mode="json"),
             )
             return Order(**inbox.result)
-        ord = Order(**order.model_dump())
+        return
+
+    async def _transactional_save(
+        self,
+        order_request: CreateOrderRequest,
+    ) -> Order:
         dto = self.uow.order_repo.CreateDTO(
-            user_id=ord.user_id,
-            item_id=ord.item_id,
-            quantity=ord.quantity,
-        )
-        out = Outbox(
-            event_type="create_order", payload=ord.model_dump(mode="json")
+            user_id=order_request.user_id,
+            item_id=order_request.item_id,
+            quantity=order_request.quantity,
         )
         outbox_dto = self.uow.outbox_repo.CreateDTO(
-            event_type=out.event_type,
-            payload=out.payload,
-            status=out.status,
-            created_at=out.created_at,
+            event_type="create_order",
+            payload=order_request.model_dump(mode="json"),
+            status=OutboxStatus.PENDING,
+            created_at=datetime.now(),
         )
         try:
             async with self.uow as u:
-                result = await u.order_repo.create(dto)
+                created_order = await u.order_repo.create(dto)
                 await u.outbox_repo.create_outbox(outbox_dto)
                 inbox_dto = u.inbox_repo.CreateDTO(
-                    idempotency_key=order.idempotency_key,
-                    payload=order.model_dump(mode="json"),
-                    result=result.model_dump(mode="json"),
+                    idempotency_key=order_request.idempotency_key,
+                    payload=order_request.model_dump(mode="json"),
+                    result=created_order.model_dump(mode="json"),
                 )
                 await u.inbox_repo.save(inbox_dto)
         except Exception as e:
             raise e
-
-        return result
+        return created_order
