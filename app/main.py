f7dcde34c73a13a84e2960ee78cf5b2ff646f8b9
anor23 <burgchiken@gmail.com>
2026-06-15 16:38:23 +0300

    feat: add lifespan to start outbox worker

--- app/main.py
+++ app/main.py
@@ -1,14 +1,33 @@
+import asyncio
 import logging
 
 from fastapi import FastAPI
 
+from app.infrastructure.kafka.producer import KafkaProducer
+from app.infrastructure.workers.outbox_worker import OutBoxWorker
 from app.presentation.midlewares import ErrorHandlingMiddleware
 from app.presentation.routers import router
 
 logger = logging.getLogger(__name__)
 
 
-app = FastAPI()
+async def lifespan(app: FastAPI):
+    logger.info("Запуск Outbox worker")
+    producer = KafkaProducer()
+    worker = OutBoxWorker(broker=producer)
+    logger.info("Outbox worker запущен")
+
+    worker_task = asyncio.create_task(worker.work(delay=10))
+
+    yield
+
+    worker_task.cancel()
+    await producer.stop()
+    logger.info("Outbox worker остановлен")
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.add_middleware(ErrorHandlingMiddleware)
 # app.add_middleware(MetricsMiddleware)
 app.include_router(router)
