8a626f468f30fdd31730fccf3cf0c08dd52ece98
anor23 <burgchiken@gmail.com>
2026-06-17 23:09:49 +0300

    feat: add notification service to OrderProcessor

--- app/application/processors.py
+++ app/application/processors.py
@@ -1,12 +1,25 @@
+from typing import Any
 from uuid import UUID
 
-from app.core.interfaces import UnitOfWorkConsumer
+from app.core.interfaces import NotificationService, UnitOfWorkConsumer
+from app.core.schemas.entities import NotificationBody
 from app.core.schemas.statuses import OrderStatus
 
 
 class OrderProcessor:
-    def __init__(self, uow: UnitOfWorkConsumer):
+    def __init__(
+        self,
+        uow: UnitOfWorkConsumer,
+        notification_service: NotificationService,
+    ):
         self.uow = uow
+        self.notify = notification_service
+
+    async def check_inbox(self, key: str) -> None | dict[str, Any]:
+        async with self.uow as uow:
+            inbox = await uow.inbox_repo.get_by_idempotency_key(key)
+        if inbox:
+            return inbox.result
 
     async def process_shipping_callback(
         self, status: OrderStatus, order_id: UUID
@@ -14,3 +27,6 @@ class OrderProcessor:
         async with self.uow as uow:
             await uow.order_repo.set_order_status(status, str(order_id))
             await uow.commit()
+
+    async def sent_notify(self, notify_body: NotificationBody):
+        await self.notify.send_notification(notify_body)
