d7a2dd2103388474ca924bbc0752d355c486908d
anor23 <burgchiken@gmail.com>
2026-06-17 18:57:57 +0300

    refactor: fix to reuse db connection

--- app/infrastructure/kafka/consumer.py
+++ app/infrastructure/kafka/consumer.py
@@ -18,25 +18,17 @@ from app.infrastructure.db.unit_of_work import UnitOfWorkConsumerImpl
 
 class KafkaConsumer:
     def __init__(self):
-        self.consumer = None
+        api_logger.info("ee1e")
+        self.consumer = self.create_consumer()
         self.order_processor = None
 
-    async def init_consumer(self):
-        api_logger.info("инициализация consumer")
-        self._session = AsyncSession()
-        order_repo = OrderRepositoryImpl(self._session)
-        uow = UnitOfWorkConsumerImpl(order_repo, self._session)
+    async def init_services(self, session: AsyncSession):
+        order_repo = OrderRepositoryImpl(session)
+        uow = UnitOfWorkConsumerImpl(order_repo, session)
         self.order_processor = OrderProcessor(uow)
         self.notify = NotificationServiceImpl()
-        try:
-            await self.start()
-        except Exception as e:
-            api_logger.error("Ошибка инциализации сonsumer %s", e)
-            return False
-        else:
-            return True
 
-    async def create_consumer(self):
+    def create_consumer(self):
         self.consumer = AIOKafkaConsumer(
             "student_system-shipment.events",
             bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
@@ -46,8 +38,6 @@ class KafkaConsumer:
         return self.consumer
 
     async def start(self):
-        if self.consumer is None:
-            await self.create_consumer()
         await self.consumer.start()
 
     async def stop(self):
@@ -55,37 +45,39 @@ class KafkaConsumer:
             await self.consumer.stop()
 
     async def consume(self):
-        await self.init_consumer()
+        await self.start()
         api_logger.info("consumer слушает")
         async for msg in self.consumer:
-            event_data = msg.value
-            event_type = event_data.get("event_type")
-            order_id = event_data.get("order_id")
+            async with AsyncSession() as session:
+                await self.init_services(session)
+                event_data = msg.value
+                event_type = event_data.get("event_type")
+                order_id = event_data.get("order_id")
 
-            if not order_id:
-                continue
-            api_logger.info("получен order_id %s", order_id)
-            if event_type == "order.shipped":
-                status = OrderStatus.SHIPPED
-                message = "Order is shipped"
+                if not order_id:
+                    continue
+                api_logger.info("получен order_id %s", order_id)
+                if event_type == "order.shipped":
+                    status = OrderStatus.SHIPPED
+                    message = "Order is shipped"
 
-            elif event_type == "order.cancelled":
-                status = OrderStatus.CANCELLED
-                message = "order is cancelled"
-            else:
-                continue
-            try:
-                await self.order_processor.process_shipping_callback(
-                    status, UUID(order_id)
-                )
-                api_logger.info("отправка уведомления косюмер")
-                notify_body = NotificationBody(
-                    message=message,
-                    reference_id=str(order_id),
-                    idempotency_key=str(order_id),
-                )
-                await self.notify.send_notification(notify_body)
-            except Exception as e:
-                api_logger.error("сonsumer Error %s", e)
-                continue
-            api_logger.info("сonsumer не слушает")
+                elif event_type == "order.cancelled":
+                    status = OrderStatus.CANCELLED
+                    message = "order is cancelled"
+                else:
+                    continue
+                try:
+                    await self.order_processor.process_shipping_callback(
+                        status, UUID(order_id)
+                    )
+                    api_logger.info("отправка уведомления косюмер")
+                    notify_body = NotificationBody(
+                        message=message,
+                        reference_id=str(order_id),
+                        idempotency_key=str(order_id),
+                    )
+                    await self.notify.send_notification(notify_body)
+                except Exception as e:
+                    api_logger.error("сonsumer Error %s", e)
+                    continue
+                api_logger.info("сonsumer не слушает")
--- app/infrastructure/workers/outbox_worker.py
+++ app/infrastructure/workers/outbox_worker.py
@@ -1,26 +1,27 @@
 import asyncio
 import json
 
+from aiokafka.structs import RecordMetadata
+
+from app.core.logs_config import api_logger
 from app.infrastructure.db.db_config import AsyncSession
 from app.infrastructure.db.repository import OutboxRepositoryImpl
 from app.infrastructure.db.unit_of_work import (
     UnitOfWorkWorker,
 )
 from app.infrastructure.kafka.producer import KafkaProducer
-from app.presentation.logs_config import api_logger
 
 
 class OutBoxWorker:
     def __init__(self, broker: KafkaProducer):
         self._broker = broker
 
-    async def _init_unitOfwork(self):
-        async with AsyncSession() as session:
-            outbox = OutboxRepositoryImpl(session)
-            return UnitOfWorkWorker(
-                session=session,
-                outbox_repo=outbox,
-            )
+    async def _init_unitOfwork(self, session: AsyncSession):
+        outbox = OutboxRepositoryImpl(session)
+        return UnitOfWorkWorker(
+            session=session,
+            outbox_repo=outbox,
+        )
 
     async def init_worker(self):
         try:
@@ -31,46 +32,57 @@ class OutBoxWorker:
         else:
             return True
 
+    async def broker_send(
+        self, message: str, idempotency_key: str
+    ) -> RecordMetadata | None:
+        try:
+            result = await self._broker.send_to_kafka(
+                topic="student_system-order.events",
+                value=str(message),
+                key=str(idempotency_key),
+            )
+
+        except Exception as e:
+            api_logger.error("Ошибка отправки в кафку %s", e)
+            return
+        else:
+            api_logger.info(
+                "отправка в кафку статус %s %s",
+                result,
+                type(result),
+            )
+        return result
+
     async def process(self):
-        unit_of_work = await self._init_unitOfwork()
-        async with unit_of_work as uow:
-            records = await uow.outbox_repo.get_records()
-            ids = []
+        async with AsyncSession() as session:
+            unit_of_work = await self._init_unitOfwork(session)
+            async with unit_of_work as uow:
+                records = await uow.outbox_repo.get_records()
+                ids = []
+                for record in records:
+                    data_to_send = record.payload
+                    idempotency_key = data_to_send.get("idempotency_key")
 
-            for record in records:
-                data_to_send = record.payload
-                idempotency_key = data_to_send.get("idempotency_key")
-                event_type = data_to_send["event_type"]
-                data_to_send["event_type"] = "order." + event_type.lower()
-                message = json.dumps(record.payload)
-                api_logger.info(message)
-                try:
-                    result = await self._broker.send_to_kafka(
-                        topic="student_system-order.events",
-                        value=str(message),
-                        key=str(idempotency_key),
-                    )
+                    event_type = data_to_send.get("event_type")
+                    data_to_send["event_type"] = "order." + event_type.lower()
+                    message = json.dumps(record.payload)
+                    api_logger.info(message)
 
-                except Exception as e:
-                    api_logger.error("Ошибка отправки в кафку %s", e)
-                    continue
-                else:
-                    api_logger.info("e")
-                    api_logger.info(
-                        "отправка в кафку статус %s %s",
-                        result,
-                        type(result),
+                    result = await self.broker_send(
+                        message, str(idempotency_key)
                     )
-                ids.append(str(record.id))
-            if ids:
-                await uow.outbox_repo.set_sent_status(ids)
+                    if not result:
+                        continue
+                    ids.append(str(record.id))
+                if ids:
+                    await uow.outbox_repo.set_sent_status(ids)
 
     async def work(self, delay=20):
         init = await self.init_worker()
+
         while not init:
             init = await self.init_worker()
             await asyncio.sleep(delay * 60)
         while True:
             await self.process()
-
             await asyncio.sleep(delay=delay)
